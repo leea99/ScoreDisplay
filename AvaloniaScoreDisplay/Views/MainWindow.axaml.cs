8d6683a553a86d8c5a5a36763e429808c3c3ee10
Avery Lee <averyl@tmstime.com>
2023-05-18 16:49:09 -0500

    Textbox changes and fixed the sizing.

--- AvaloniaScoreDisplay/Views/MainWindow.axaml.cs
+++ AvaloniaScoreDisplay/Views/MainWindow.axaml.cs
@@ -14,6 +14,7 @@ namespace AvaloniaScoreDisplay.Views
         public MainWindow()
         {
             InitializeComponent();
+            WindowState = WindowState.Maximized;
             GetSportsData();
         }
 
