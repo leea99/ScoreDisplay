6c6cd16606332d2bf385935faf396592f1b0c492
Avery Lee <averyl@tmstime.com>
2023-05-22 15:11:43 -0500

    Added Logging.

--- AvaloniaScoreDisplay/App.axaml.cs
+++ AvaloniaScoreDisplay/App.axaml.cs
@@ -3,11 +3,14 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using AvaloniaScoreDisplay.ViewModels;
 using AvaloniaScoreDisplay.Views;
+using log4net;
+using log4net.Config;
 
 namespace AvaloniaScoreDisplay
 {
     public partial class App : Application
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(App));
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
@@ -24,6 +27,8 @@ namespace AvaloniaScoreDisplay
             }
 
             base.OnFrameworkInitializationCompleted();
+            XmlConfigurator.Configure(); // Load the Log4Net configuration from log4net.config
+            log.Debug("Application started");
         }
     }
 }
--- AvaloniaScoreDisplay/Views/MainWindow.axaml.cs
+++ AvaloniaScoreDisplay/Views/MainWindow.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia.Controls;
 using AvaloniaScoreDisplay.Models;
 using AvaloniaScoreDisplay.Scoreboards;
+using log4net;
+using log4net.Config;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -8,13 +10,16 @@ using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace AvaloniaScoreDisplay.Views
-{
+{ 
     public partial class MainWindow : Window
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(App));
         public MainWindow()
         {
+            log.Debug("Main Window");
             InitializeComponent();
             WindowState = WindowState.Maximized;
+            XmlConfigurator.Configure();
             GetSportsData();
         }
 
@@ -28,25 +33,40 @@ namespace AvaloniaScoreDisplay.Views
 
         private async Task GetMLBData()
         {
-            using (var client = new HttpClient())
+            try
             {
-                var response = await client.GetAsync("http://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard");
-                var content = await response.Content.ReadAsStringAsync();
-                MLBData baseballData = JsonConvert.DeserializeObject<MLBData>(content);
-                List<MLB> graphics = new List<MLB>();
-                foreach (var game in baseballData.events)
+                using (var client = new HttpClient())
                 {
-                    graphics.Add(await new MLB().GetMLBGameScore(game));
-                }
-                foreach (var g in graphics)
-                {
-                    try
+                    var response = await client.GetAsync("http://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard");
+                    var content = await response.Content.ReadAsStringAsync();
+                    MLBData baseballData = JsonConvert.DeserializeObject<MLBData>(content);
+                    List<MLB> graphics = new List<MLB>();
+                    foreach (var game in baseballData.events)
                     {
-                        Content = g;
-                        await Task.Delay(5000);
+                        try
+                        {
+                            log.Debug(game.id);
+                            graphics.Add(await new MLB().GetMLBGameScore(game));
+                        }
+                        catch (Exception ex)
+                        {
+                            log.Error("Game ID: " + game.id + " " + ex.Message);
+                        }
                     }
-                    catch (Exception ex) { }
+                    foreach (var g in graphics)
+                    {
+                        try
+                        {
+                            Content = g;
+                            await Task.Delay(5000);
+                        }
+                        catch (Exception ex) { }
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error getting MLB game data: " + ex.Message);
             }
         }
     }
